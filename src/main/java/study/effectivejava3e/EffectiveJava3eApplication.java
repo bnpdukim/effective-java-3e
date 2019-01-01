b3e1464016f65b7d06201097310ba71f3d3d8003
sajacaros <sajacall82@naver.com>
2019-01-02 00:16:36 +0900

    initial

---
+++ src/main/java/study/effectivejava3e/EffectiveJava3eApplication.java
@@ -0,0 +1,14 @@
+package study.effectivejava3e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveJava3eApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveJava3eApplication.class, args);
+    }
+
+}
+
