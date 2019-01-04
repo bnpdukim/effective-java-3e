3765ec0c85aabe4a51ab55b664ad958ffe1663e9
dukim <dukim@bnpinnovation.com>
2019-01-04 11:57:00 +0900

    item03 supplier 예제 추가

--- src/main/java/study/effective/ch02/item03/Item03.java
+++ src/main/java/study/effective/ch02/item03/Item03.java
@@ -1,9 +1,27 @@
 package study.effective.ch02.item03;
 
+import java.util.function.Supplier;
+
 public class Item03 {
     public static void main(String[] args ) {
+        // static member
         Elvis.INSTANCE.leaveTheBuilding();
 
+        // static factory method
         Tom.getInstance().leaveTheBuilding();
+
+        // supplier
+        doSomething(Tom::getInstance);
+        doSomething(()->Tom.getInstance());
+        doSomething(new Supplier<Tom>() {
+            @Override
+            public Tom get() {
+                return Tom.getInstance();
+            }
+        });
+    }
+
+    private static void doSomething(Supplier<Tom> tom) {
+        tom.get().leaveTheBuilding();
     }
 }
