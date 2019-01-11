77c681d4f4b520567e7603713a39c8ad993af971
dukim <dukim@bnpinnovation.com>
2019-01-11 15:22:09 +0900

    ch04

---
+++ src/main/java/study/effective/ch04/Item15.java
@@ -0,0 +1,29 @@
+package study.effective.ch04;
+
+public class Item15 {
+    private static class Coffee {
+        private final String name;
+
+        private Coffee(String name) {
+            this.name = name;
+        }
+    }
+
+    private class Coffee2 {
+        private final String name;
+
+        private Coffee2(String name) {
+            this.name = name;
+        }
+    }
+
+    public static void main(String[] args) {
+        // static vs non static class
+        // http://www.java67.com/2012/10/nested-class-java-static-vs-non-static-inner.html
+        new Coffee("라떼");
+
+//        new Coffee2("냠냠"); // error
+        Item15 item15 = new Item15();
+        Item15.Coffee2 coffee2 =  item15.new Coffee2("아메리카노");
+    }
+}
