e941d396274c9635bc7b085e0d00f4ca75226354
dukim <dukim@bnpinnovation.com>
2019-01-18 16:12:32 +0900

    소스 추가

--- src/main/java/study/effective/ch04/item15/Item15.java
+++ src/main/java/study/effective/ch04/item15/Item15.java
@@ -5,6 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Item15 {
     public static void main(String[] args) {
+        // 접근 권한 없음
+//        CoffeeShop.Coffee 에러커피 = new CoffeeShop.CoffeeStatic("에러커피");
+
+
         // static vs non static class
         // http://www.java67.com/2012/10/nested-class-java-static-vs-non-static-inner.html
         CoffeeShop.Coffee 라떼  = CoffeeShop.makeCoffee("라떼", true);
