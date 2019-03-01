4165945d3a9153cc2faf68a3e15af8990b7df834
sajacaros <sajacall82@naver.com>
2019-03-02 03:12:58 +0900

    item59 addeD

---
+++ src/main/java/study/effective/ch09/Item59.java
@@ -0,0 +1,23 @@
+package study.effective.ch09;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Random;
+
+@Slf4j
+public class Item59 {
+    public static void main(String[] args) {
+        int n = 2*(Integer.MAX_VALUE / 3);
+        int low = 0;
+        for(int i=0;i<100000;i++)
+            if(random(n) < n/2)
+                low++;
+        log.info("{}",low);
+    }
+
+    static Random rnd = new Random();
+    private static int random(int n) {
+        return Math.abs(rnd.nextInt()) % n;
+//        return rnd.nextInt(n);
+    }
+}
