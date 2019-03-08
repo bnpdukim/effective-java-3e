5321d4597e526ae45d19ec88132baaa595754c57
sajacaros <sajacall82@naver.com>
2019-03-09 08:38:39 +0900

    item76 complete

---
+++ src/main/java/study/effective/ch10/item76/Item76.java
@@ -0,0 +1,9 @@
+package study.effective.ch10.item76;
+
+import java.util.EmptyStackException;
+
+public class Item76 {
+    public static void main(String[] args) {
+        EmptyStackException e;
+    }
+}
